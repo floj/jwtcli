5e95c10835f34e10c563c02b5b7f080c3f4e9a01
floj <floj@users.noreply.github.com>
2023-09-21 08:53:10 +0200

    quick and dirty jwt decoder

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/floj/jwtcli
+
+go 1.21.1
+
+require github.com/hokaccha/go-prettyjson v0.0.0-20211117102719-0474bc63780f
+
+require (
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/hokaccha/go-prettyjson"
+)
+
+func main() {
+	tokens := [][]byte{}
+	if len(os.Args) == 1 {
+		jwt, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could read from stdin: %v", err)
+			os.Exit(1)
+		}
+		tokens = append(tokens, jwt)
+	} else {
+		for _, t := range os.Args[1:] {
+			tokens = append(tokens, []byte(t))
+		}
+	}
+
+	for _, t := range tokens {
+		if err := printJwt(t, os.Stdout); err != nil {
+			fmt.Fprintf(os.Stderr, "could not parse JWT: %v", err)
+			os.Exit(1)
+		}
+	}
+
+}
+
+func printJwt(jwt []byte, out io.Writer) error {
+	dots := bytes.Count(jwt, []byte{'.'})
+	if dots != 2 {
+		return fmt.Errorf("jwt must contain exactly 2 dots, but found %d", dots)
+	}
+	parts := bytes.Split(jwt, []byte{'.'})
+	// 0 = header, 1 = payload, 2 = signature
+
+	pjson := prettyjson.NewFormatter()
+
+	for partType, part := range map[string][]byte{"header": parts[0], "payload": parts[1]} {
+		dec := make([]byte, len(part))
+		n, err := base64.URLEncoding.Decode(dec, part)
+		if err != nil {
+			return fmt.Errorf("failed to url-decode %s: %w", partType, err)
+		}
+		dec = dec[:n]
+		obj := map[string]any{}
+		err = json.Unmarshal(dec, &obj)
+		if err != nil {
+			return fmt.Errorf("failed to parse json of %s: %w", partType, err)
+		}
+		s, err := pjson.Marshal(obj)
+		if err != nil {
+			return err
+		}
+		if _, err := out.Write(s); err != nil {
+			return err
+		}
+		if _, err := out.Write([]byte{'\n'}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
